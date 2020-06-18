4df724c36812efe3fa88a79ae9982d7d85f30d03
kabilangr <g.r.kabilan@gmail.com>
2020-06-18 18:53:23 +0530

    code along 1 completed

---
+++ box.js
@@ -0,0 +1,17 @@
+let button = document.getElementById("boxes");
+button.addEventListener("click", myfun);
+var oldValue = 0;
+var i = 1;
+
+function myfun() {
+    let n = document.getElementById('number').value;
+    n = parseInt(oldValue) + parseInt(n);
+    for (; i <= n; i++) {
+        var box = document.createElement('div');
+        box.classList.add('myDiv');
+        document.getElementById('box').appendChild(box);
+        box.innerHTML = i;
+        oldValue = document.getElementById('box').lastElementChild.innerHTML;
+        console.log(oldValue);
+    }
+}
